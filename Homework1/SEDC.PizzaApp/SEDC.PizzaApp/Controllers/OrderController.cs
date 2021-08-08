03843b4d9e895961efc80d4af1177ba99cd172ef
Filip-tr-08 <filiptrajo@yahoo.com>
2021-08-09 00:42:30 +0200

    Homework1

---
+++ Homework1/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("Orders")]
+        public IActionResult Index()
+        {
+            List<Order> orders = StaticDb.Orders;
+            return View(orders);
+        }
+        [Route("Order/Details/{id?}")]
+    public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            else
+            {
+                Order foundOrder = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+                if (foundOrder == null)
+                {
+                    return new EmptyResult();
+                }
+                else
+                {
+                    return View(foundOrder);
+                }
+            }
+        }
+        [Route("Order/JsonData")]
+        public IActionResult GetJsonOrder()
+        {
+            Order order = new Order()
+            {
+                Id = 1,
+                IsDelivery = false,
+                PizzasOrdered = StaticDb.Pizzas
+            };
+            return new JsonResult(order);
+        }
+
+
+        public IActionResult BackToIndexHomeController()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
