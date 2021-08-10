0c789c0567eb02b8a960c119b975283c6ba24786
Filip-tr-08 <filiptrajo@yahoo.com>
2021-08-11 01:37:33 +0200

    Homework2

---
+++ Homework2/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.PizzaApp.Models.Mappers;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Index page";
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            List<PizzaDetailsViewModel> pizzaViewModels = pizzas.Select(x => x.PizzaToPizzaDetailsViewModel()).ToList();
+            return View(pizzaViewModels); 
+        }
+
+        public IActionResult Details(int? id)
+        {
+            ViewData["Title"] = "Details";
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if(pizza == null)
+            {
+                return new EmptyResult();
+            }
+            ViewBag.PizzaViewModel = pizza.PizzaToPizzaDetailsViewModel();
+            return View();
+        }
+        [Route("SeePizzas")] 
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
