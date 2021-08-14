92d6cd876f1b8f283177da6f16fd48417c94c430
Filip-tr-08 <filiptrajo@yahoo.com>
2021-08-14 12:46:18 +0200

    Homework3

---
+++ Homework3/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.PizzaApp.Models.Mappers;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Index page";
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            List<PizzaDetailsViewModel> pizzaViewModels = pizzas.Select(x => x.PizzaToPizzaDetailsViewModel()).ToList();
+            return View(pizzaViewModels); 
+        }
+
+        public IActionResult Details(int? id)
+        {
+            ViewData["Title"] = "Details";
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if(pizza == null)
+            {
+                return new EmptyResult();
+            }
+            PizzaDetailsViewModel pizzaViewModel = pizza.PizzaToPizzaDetailsViewModel();
+            ViewBag.PizzaViewModel = pizzaViewModel;
+            return View(pizzaViewModel);
+        }
+        [Route("SeePizzas")] 
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Homework3/SEDC.PizzaApp/SEDC.PizzaApp/Models/Mappers/PizzaMapper.cs
@@ -0,0 +1,24 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class PizzaMapper
+    {
+        public static PizzaDetailsViewModel PizzaToPizzaDetailsViewModel(this Pizza pizza)
+        {
+            return new PizzaDetailsViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                PizzaSize = pizza.PizzaSize,
+                Price = pizza.HasExtra ? pizza.Price + 10 : pizza.Price,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+        }
+    }
+}
